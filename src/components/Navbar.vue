1ac91dfab198735f5fa42d45484b564ccfe21eed
victor-marciano <vpm.marciano@gmail.com>
2025-02-21 20:16:27 -0300

    adding tailwind, usevue and eslint

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class="flex dark:bg-gray-900 bg-gray-100">
+        <ToggleThemeButton></ToggleThemeButton>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ToggleThemeButton from './ToggleThemeButton.vue';
+
+export default defineComponent({
+    name: 'NavbarMain',
+    components: {
+        ToggleThemeButton,
+    },
+    setup() {
+        return {};
+    },
+});
+</script>
+<style lang=""></style>
---
+++ src/components/ToggleThemeButton.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <!-- Main content -->
+    <div class="flex items-center justify-center">
+        <button
+            @click="toggleDark()"
+            class="px-4 py-2 text-white bg-blue-500 rounded-e-3xl hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800 transition-colors"
+        >
+            <!-- {{ isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode' }} -->
+            <svg v-if="isDark" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+            </svg>
+            <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+            </svg>
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core';
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<style lang=""></style>
